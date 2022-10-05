f826212ecaaa3a3727d64291ad3e8a46f77530bf
rickyalvedro <pintusiraj786@gmail.com>
2022-10-05 22:43:52 +0530

    getElementsByClassName

---
+++ Gitstart/getelementsbyclassname.js
@@ -0,0 +1,14 @@
+// GET ELEMENTS BY CLASS NAME//
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// GIVES ERROR
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0 ; i < items.length ; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
